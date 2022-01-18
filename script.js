8a427744a4ae4f5cf7de9bc2d8c811ba82dd1de6
Jo Anna Mollman <mommollman@gmail.com>
2022-01-18 13:00:00 -0800

    added a copy to clipboard feature

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+//bring in DOM elements
 const btn = document.getElementById('btn');
-const getAJoke = document.getElementById('joke')
+const getAJoke = document.getElementById('joke');
 
+
+//get a new joke
+btn.addEventListener('click', getJoke);
+
+//url to get dad joke
 const url = "https://icanhazdadjoke.com/"
-btn.addEventListener('click', getJoke)
 
-// getJoke()
-
+//async function
 async function getJoke() {
+  //headers
   const headerDetails = 
         {headers: {
-          "Accept": "application/json"
+          "Accept": "application/json",
+          "User-Agent": "My Library (https://github.com/jodi08/dadjoke/https://icanhazdadjoke.com/"
         }
       }
-  
+  //await response
+
   let response = await fetch(url, 
   headerDetails)
-  let data = await response.json()
+  let data = await response.json();
 
-  getAJoke.innerHTML = data.joke
-
+  getAJoke.innerHTML = data.joke;
 }
+//call here so that it loads with a joke, so an empty div can't be copied.
+getJoke()
 
+function copyText() {
   
+  // get the text field
+  let copyJoke = getAJoke.innerHTML;
+  
+  
+  //copy to clipboard
+
+  navigator.clipboard.writeText(copyJoke)
+    .then(() => alert("Joke copied!"))
+    .catch((error) => alert("Copy failed"))
+}
